e6e194400d744079a6c175ce062cf367f645a73c
Marsmanleo <marstvam@gmail.com>
2023-02-13 03:58:02 +0800

    initialize project

---
+++ src/data/menuData.js
@@ -0,0 +1,17 @@
+export const menuData = [
+  { title: "Courses", icon: "/images/icons/courses.svg", link: "/courses" },
+  {
+    title: "Tutorials",
+    icon: "/images/icons/Tutorials.svg",
+    link: "/Tutorials",
+  },
+  { title: "Pricing", icon: "/images/icons/Pricing.svg", link: "/Pricing" },
+  { title: "", icon: "/images/icons/search.svg", link: "/search" },
+  { title: "", icon: "/images/icons/account.svg", link: "/account" },
+]
+
+export const tooltipsData = [
+  { title: "Profile", icon: "/images/icons/profile.svg", link: "/profile" },
+  { title: "Settings", icon: "/images/icons/settings.svg", link: "/settings" },
+  { title: "Sign out", icon: "/images/icons/signout.svg", link: "/logout" },
+]
